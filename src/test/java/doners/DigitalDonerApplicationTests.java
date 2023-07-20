b86a72e52fc87e99508e2165f159a63dfff3e525
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2023-07-20 15:18:07 +0600

    Spring application

---
+++ src/test/java/doners/DigitalDonerApplicationTests.java
@@ -0,0 +1,13 @@
+package doners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalDonerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
