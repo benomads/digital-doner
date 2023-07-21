00ffac5316fbf3f882f357cd91d3a92223b6c401
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2023-07-21 20:13:26 +0600

    update

---
+++ src/main/java/doners/Doner.java
@@ -0,0 +1,18 @@
+package doners;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Doner {
+    private String name;
+    private List<Ingredient> ingredients;
+
+}
---
+++ src/main/java/doners/web/DesignDonerController.java
@@ -0,0 +1,68 @@
+package doners.web;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import doners.DonerOrder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import lombok.extern.slf4j.Slf4j;
+import doners.Ingredient;
+import doners.Ingredient.Type;
+import doners.Doner;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("donerOrder")
+public class DesignDonerController {
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("BEME", "Beef Meat", Type.PROTEIN),
+                new Ingredient("CHFI", "Chicken Fillet", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("CUBR", "Cucumbers", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("KTUP", "Ketchup", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+            filterByType(ingredients, type));
+        }
+    }
+
+    @ModelAttribute(name = "donerOrder")
+    public DonerOrder order() {
+        return new DonerOrder();
+    }
+    @ModelAttribute(name = "doner")
+    public Doner doner() {
+        return new Doner();
+    }
+    @GetMapping
+    public String showDesignForm() {
+        return "design";
+    }
+
+    private Iterable<Ingredient> filterByType(
+            List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+}
