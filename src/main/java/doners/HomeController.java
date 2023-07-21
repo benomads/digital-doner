c90e9ffbc120e8f37ad9abd05188d137fd533cc4
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2023-07-21 20:04:43 +0600

    Spring application

--- src/main/java/doners/HomeController.java
+++ src/main/java/doners/HomeController.java
@@ -2,8 +2,10 @@ package doners;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/digital-doner")
 public class HomeController {
 
     @GetMapping("/")
