b14c9518462e387737d61337aaa1753186a33c6b
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2023-08-09 22:58:57 +0600

    Spring application update

--- src/main/java/doners/web/api/IngredientController.java
+++ src/main/java/doners/web/api/IngredientController.java
@@ -4,6 +4,7 @@ import doners.data.IngredientRepository;
 import doners.entity.Ingredient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,12 +24,14 @@ public class IngredientController {
     }
 
     @PostMapping
+    @PreAuthorize("#hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
     public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
         return ingredientRepository.save(ingredient);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("#hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteIngredient(@PathVariable("id") String ingredientId) {
         ingredientRepository.deleteById(ingredientId);
