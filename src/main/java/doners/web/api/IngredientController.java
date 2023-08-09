2e4da2140bc0597ded19c6cf909c6ecd6c5517ac
Akzhol Yeginbay <eginbaiakzhol@gmail.com>
2023-08-09 22:53:17 +0600

    Spring application update

---
+++ src/main/java/doners/web/api/IngredientController.java
@@ -0,0 +1,36 @@
+package doners.web.api;
+
+import doners.data.IngredientRepository;
+import doners.entity.Ingredient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/ingredients", produces = "application/json")
+@CrossOrigin(origins = "http://localhost:8080")
+public class IngredientController {
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public IngredientController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @GetMapping
+    public Iterable<Ingredient> allIngredients() {
+        return ingredientRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
+        return ingredientRepository.save(ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteIngredient(@PathVariable("id") String ingredientId) {
+        ingredientRepository.deleteById(ingredientId);
+    }
+}
